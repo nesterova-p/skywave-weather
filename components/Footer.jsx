b1eb5962c396d2227b764d607a588561f257b020
nott-po <nesterovapolina55@gmail.com>
2025-03-03 00:30:01 +0100

    Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,7 +1,20 @@
 export default function Footer() {
     return (
-        <footer>
+        <footer className="py-4 flex justify-center pb-8 flex-col items-center justify-center w-full">
             <p>© {new Date().getFullYear()} SkyWave</p>
+
+            <p className="footer-text text-sm flex items-center gap-1">
+                Made by
+                <a
+                    href="https://github.com/nott-po/skywave-weather"
+                    target="_blank"
+                    className=" text-rose-400 font-bold"
+                >
+                    Polina Nott
+                </a>
+            </p>
         </footer>
+
+
     );
 }
