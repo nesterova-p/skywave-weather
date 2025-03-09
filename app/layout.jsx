3189aa6b3f8f19f950b1fb5d10d66e1ec4f54d5c
nott-po <nesterovapolina55@gmail.com>
2025-03-09 18:21:40 +0100

    - Analitics

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,6 +3,7 @@ import NavBar from "../components/Navbar";
 import Footer from "../components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next"
 import ThemeProviders from "../Providers/ThemeProviders.jsx";
+import { Analytics } from "@vercel/analytics/react"
 
 export default function RootLayout({ children }) {
     return (
@@ -19,6 +20,7 @@ export default function RootLayout({ children }) {
                 {children}
                 <Footer/>
                 <SpeedInsights />
+                <Analytics />
             </main>
         </ThemeProviders>
         </body>
